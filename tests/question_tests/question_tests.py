7f62526d91119e2c952b62736778d35db8f857f3
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-16 20:55:59 -0500

    Update question_tests.py

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -10,3 +10,14 @@ class TestUseLocalVariable(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+import unittest
+from src.question_2.get_sum_of_evens import get_sum_of_evens
+
+class TestGetSumOfEvens(unittest.TestCase):
+    def test_values(self):
+        self.assertEqual(get_sum_of_evens(11), 30)
+        self.assertEqual(get_sum_of_evens(10), 30)
+        self.assertEqual(get_sum_of_evens(8), 20)
+
+if __name__ == '__main__':
+    unittest.main()
