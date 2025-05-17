55377609c7be6e05778a1d89b81ac897193ab37b
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-16 20:52:26 -0500

    Update question_b.py

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,5 @@
-#write functions here, don't add input('') statements here!
+    """Return the sum of even numbers from 1 to num (inclusive)"""
+    total = 0
+    for i in range(2, num + 1, 2):  # Start from 2, step by 2 (even numbers)
+        total += i
+    return total 
