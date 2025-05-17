a40b34c12a1f4ac6323d1a329fc3afde26b572d8
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-16 20:45:32 -0500

    Update main.py

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,4 +1,10 @@
-def use_local_variable(num):
-    # Local variable `num` inside the function
-    num = 10
-    print(f"Inside function, num is set to {num}")
+from use_local_variable import use_local_variable
+
+def main():
+    num = 100
+    print(f"Before function call, num = {num}")
+    use_local_variable(num)
+    print(f"After function call, num = {num}")
+
+if __name__ == "__main__":
+    main()
